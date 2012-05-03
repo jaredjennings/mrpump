1a0d6dd69e3e14337f25293039b4fcb1c024defc
jared jennings <jjenning@fastmail.fm>
2012-05-02 22:42:37 -0500

    fix up import errors and missing methods

--- golem/auth.py
+++ golem/auth.py
@@ -1,15 +1,15 @@
 import tweepy
 
 def get_api(config):
-    consumer_key = config.item('app', 'key')
-    consumer_secret = config.item('app', 'secret')
+    consumer_key = config.get('app', 'key')
+    consumer_secret = config.get('app', 'secret')
 
-    me = config.item('global', 'screen name')
-    me_oauth_token = config.item(me, 'token')
-    me_oauth_secret = config.item(me, 'secret')
+    me = config.get('global', 'screen name')
+    me_oauth_token = config.get(me, 'token')
+    me_oauth_secret = config.get(me, 'secret')
 
     oa = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    oa.set_access_token(developer_oauth_token, developer_oauth_secret)
+    oa.set_access_token(me_oauth_token, me_oauth_secret)
 
     api = tweepy.API(oa)
     return api
